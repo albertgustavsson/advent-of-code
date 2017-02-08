52aff6158138d2f012c9cf15ff9edbc559f3e844
Albert Gustavsson <albert99gustavsson@gmail.com>
2017-02-08 09:02:41 +0100

    Added 2015 solutions in C#

---
+++ 2015/01/csharp/p1/01p1/01p1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01p1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int level = 0;
+            string line = "";
+            string[] lines = System.IO.File.ReadAllLines(@"C:\Users\albgus02\GoogleDrive\AdventOfCode\2015\01\p1\input.txt");
+            
+            for (int j = 0; j < lines.Length; j++)
+            {
+                line = lines[j];
+
+                for (int i = 0; i < line.Length; i++)
+                {
+                    if (line[i] == '(')
+                    {
+                        level++;
+                    }
+                    else if (line[i] == ')')
+                    {
+                        level--;
+                    }
+                }
+            }
+
+            Console.WriteLine("Santa claus followed the instructions to level " + level);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 2015/01/csharp/p2/01p2/01p2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01p2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int level = 0, place = 0;
+            bool cont = true;
+            string line = "";
+            string[] lines = System.IO.File.ReadAllLines(@"C:\Users\albgus02\GoogleDrive\AdventOfCode\2015\01\p2\input.txt");
+
+            while (cont == true)
+            {
+                for (int j = 0; j < lines.Length; j++)
+                {
+                    line = lines[j];
+
+                    for (int i = 0; i < line.Length; i++)
+                    {
+                        place++;
+
+                        if (line[i] == '(')
+                        {
+                            level++;
+                        }
+                        else
+                        {
+                            level--;
+                        }
+                        if (level < 0)
+                        {
+                            cont = false;
+                            break;
+                        }
+                    }
+                    if (cont == false)
+                    {
+                        break;
+                    }
+                }
+            }
+
+            Console.WriteLine("Santa claus went into the basement when following character no." + place + " of the instructions.");
+            Console.ReadKey();
+        }
+    }
+}
