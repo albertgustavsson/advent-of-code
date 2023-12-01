fc49043fe6f204ca01c981862dd0d639633bcff4
Albert Gustavsson <albert99gustavsson@gmail.com>
2023-12-01 17:50:22 +0100

    2023 day 1 improvements

--- 2023/01/kotlin/src/main/kotlin/Solution.kt
+++ 2023/01/kotlin/src/main/kotlin/Solution.kt
@@ -1,59 +1,26 @@
 package albertgustavsson.adventofcode
 
-import java.io.FileReader
+import java.io.File
+
+val digitsTranslation = mapOf(
+        Pair("1", "1"), Pair("2", "2"), Pair("3", "3"), Pair("4", "4"), Pair("5", "5"), Pair("6", "6"), Pair("7", "7"), Pair("8", "8"), Pair("9", "9"),
+        Pair("one", "1"), Pair("two", "2"), Pair("three", "3"), Pair("four", "4"), Pair("five", "5"), Pair("six", "6"), Pair("seven", "7"), Pair("eight", "8"), Pair("nine", "9"))
 
 fun main(args:Array<String>) {
+    if (args.size != 2) throw IllegalArgumentException("Required arguments: <part> <inputFileName>")
     val part = args[0].toInt()
     val inputFileName = args[1]
-    val fr = FileReader(inputFileName)
-    val lines = fr.readLines()
-    val validDigits: List<String> = if (part == 1) {
-        listOf("1", "2", "3", "4", "5", "6", "7", "8", "9")
-    } else if (part == 2) {
-        listOf("1", "2", "3", "4", "5", "6", "7", "8", "9", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
-    } else {
-        throw IllegalArgumentException("Part 1 or 2?")
+
+    val validDigits: List<String> = when (part) {
+        1 -> digitsTranslation.keys.filter { it.length == 1 && it[0].isDigit() }
+        2 -> digitsTranslation.keys.toList()
+        else -> throw IllegalArgumentException("Part 1 or 2?")
     }
-    val digitsTranslation = mapOf(Pair("one", "1"), Pair("two", "2"), Pair("three", "3"), Pair("four", "4"), Pair("five", "5"), Pair("six", "6"), Pair("seven", "7"), Pair("eight", "8"), Pair("nine", "9"))
-    val numbers = mutableListOf<Int>()
-    for (line in lines) {
-        var tempLine = line
-        var firstDigit = ""
-        while (tempLine.isNotEmpty()) {
-            var foundDigit = false
-            for (validDigit in validDigits) {
-                if (tempLine.startsWith(validDigit)) {
-                    foundDigit = true
-                    firstDigit = digitsTranslation.getOrElse(validDigit) { validDigit }
-                    break
-                }
-            }
-            if (foundDigit) {
-                break
-            } else {
-                tempLine = tempLine.substring(1)
-            }
-        }
-        tempLine = line
-        var lastDigit = ""
-        while (tempLine.isNotEmpty()) {
-            var foundDigit = false
-            for (validDigit in validDigits) {
-                if (tempLine.endsWith(validDigit)) {
-                    foundDigit = true
-                    lastDigit = digitsTranslation.getOrElse(validDigit) { validDigit }
-                    break
-                }
-            }
-            if (foundDigit) {
-                break
-            } else {
-                tempLine = tempLine.substring(0,tempLine.length-1)
-            }
-        }
-        val number = (firstDigit + lastDigit).toInt()
-        numbers.add(number)
-    }
-    val sum: Int = numbers.stream().reduce { i: Int, j: Int -> i + j }.get()
+
+    val sum = File(inputFileName).bufferedReader().lines().map { line ->
+        val firstDigit = digitsTranslation.getValue(line.findAnyOf(validDigits)!!.second)
+        val lastDigit = digitsTranslation.getValue(line.findLastAnyOf(validDigits)!!.second)
+        (firstDigit + lastDigit).toInt()
+    }.reduce { i, j -> i + j }.get()
     println(sum)
 }
