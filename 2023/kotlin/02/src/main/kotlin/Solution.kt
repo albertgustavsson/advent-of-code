66642288d53817c9870e093a59f22660ea9c8583
Albert Gustavsson <albert99gustavsson@gmail.com>
2023-12-02 07:48:08 +0100

    Solved 2023 day 2 part 1

--- 2023/kotlin/02/src/main/kotlin/Solution.kt
+++ 2023/kotlin/02/src/main/kotlin/Solution.kt
@@ -1,9 +1,34 @@
 package albertgustavsson.adventofcode.y2023.d02
 
+import java.io.File
+
+val maxNumberPerColor = mapOf(Pair("red", 12), Pair("green", 13), Pair("blue", 14))
+
 fun main(args:Array<String>) {
     if (args.size != 2) throw IllegalArgumentException("Required arguments: <part> <inputFileName>")
     val part = args[0].toInt()
     val inputFileName = args[1]
 
-    println("Hello, world!")
+    val sum = File(inputFileName).bufferedReader().lines().map { line ->
+        val parts = line.split(": ")
+        val gameId = parts[0].split(" ")[1].toInt()
+        val revealings = parts[1].split("; ")
+        var gameValid = true
+        for (revealing in revealings) {
+            val cubes = revealing.split(", ")
+            for (cube in cubes) {
+                val x = cube.split(" ")
+                val color = x[1]
+                val number = x[0].toInt()
+                if (number > maxNumberPerColor.getValue(color)) gameValid = false
+            }
+        }
+        if (gameValid) {
+            gameId
+        } else {
+            0
+        }
+    }.reduce { i, j -> i + j }.get()
+
+    println(sum)
 }
