a3b3215e67ec4fcf93512fcdb309b83e72930d32
Albert Gustavsson <albert99gustavsson@gmail.com>
2023-12-02 08:08:26 +0100

    Solved 2023 day 2 part 2

--- 2023/kotlin/02/src/main/kotlin/Solution.kt
+++ 2023/kotlin/02/src/main/kotlin/Solution.kt
@@ -1,6 +1,7 @@
 package albertgustavsson.adventofcode.y2023.d02
 
 import java.io.File
+import kotlin.math.max
 
 val maxNumberPerColor = mapOf(Pair("red", 12), Pair("green", 13), Pair("blue", 14))
 
@@ -13,20 +14,32 @@ fun main(args:Array<String>) {
         val parts = line.split(": ")
         val gameId = parts[0].split(" ")[1].toInt()
         val revealings = parts[1].split("; ")
-        var gameValid = true
+        var minRequiredCubes = mutableMapOf(Pair("red", Int.MIN_VALUE), Pair("green", Int.MIN_VALUE), Pair("blue", Int.MIN_VALUE))
         for (revealing in revealings) {
             val cubes = revealing.split(", ")
             for (cube in cubes) {
                 val x = cube.split(" ")
                 val color = x[1]
                 val number = x[0].toInt()
-                if (number > maxNumberPerColor.getValue(color)) gameValid = false
+                minRequiredCubes.set(color, max(minRequiredCubes.getValue(color), number))
             }
         }
-        if (gameValid) {
-            gameId
+        if (part == 1) {
+            var gameValid = true
+            for (entry in maxNumberPerColor) {
+                if (minRequiredCubes.getValue(entry.key) > entry.value) gameValid = false
+            }
+            if (gameValid) {
+                gameId
+            } else {
+                0
+            }
         } else {
-            0
+            var power = 1
+            for (value in minRequiredCubes.values) {
+                power *= value
+            }
+            power
         }
     }.reduce { i, j -> i + j }.get()
 
