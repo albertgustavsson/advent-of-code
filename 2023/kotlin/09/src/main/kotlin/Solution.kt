dd881968626b9c87b9cf7d0e7fd018c4db8a49c7
Albert Gustavsson <albert99gustavsson@gmail.com>
2023-12-10 22:54:16 +0100

    Solved 2023 day 9 part 1

---
+++ 2023/kotlin/09/src/main/kotlin/Solution.kt
@@ -0,0 +1,28 @@
+package albertgustavsson.adventofcode.y2023.d09
+
+import java.io.File
+
+fun main(args: Array<String>) {
+    require(args.size == 2) { throw IllegalArgumentException("Required arguments: <part> <inputFileName>") }
+    val part = args[0].toInt()
+    val inputFileName = args[1]
+
+    val sum = File(inputFileName).bufferedReader().lines().map { line ->
+        val numbers = line.split("\\s+".toRegex()).map(String::toInt).toMutableList()
+        var steps: MutableList<MutableList<Int>> = mutableListOf(numbers)
+        var temps = numbers
+        while (!temps.all { it == 0 }) {
+            temps = temps.windowed(size = 2, transform = { neighbors ->  neighbors[1] - neighbors[0]}).toMutableList()
+            steps.add(temps)
+        }
+        // Temps is now all zeroes. Add one zero and extrapolate back
+        var diff = 0
+        for (i in steps.size-1 downTo  0) {
+            diff = steps[i].last() + diff
+            steps[i].add(diff)
+        }
+        steps.first().last()
+    }.reduce(Int::plus).get()
+
+    println(sum)
+}
