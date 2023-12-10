96785d743a63346229d5c4f7162ebc437403ec1e
Albert Gustavsson <albert99gustavsson@gmail.com>
2023-12-10 15:41:49 +0100

    Solved 2023 day 8 part 2

--- 2023/kotlin/08/src/main/kotlin/Solution.kt
+++ 2023/kotlin/08/src/main/kotlin/Solution.kt
@@ -21,23 +21,66 @@ fun main(args: Array<String>) {
     }
 
     var done = false
-    var nextNode = "AAA"
-    var steps = 0
-    while (!done) {
-        instructionsLine.forEach {
-            if (!done) {
-                if (it == 'L') {
-                    nextNode = networkMap.getValue(nextNode).first
-                } else if (it == 'R') {
-                    nextNode = networkMap.getValue(nextNode).second
-                }
-                steps++
-                if (nextNode == "ZZZ") {
-                    done = true
+    var steps = 0L
+    if (part == 1) {
+        var nextNode = "AAA"
+        while (!done) {
+            instructionsLine.forEach {
+                if (!done) {
+                    if (it == 'L') {
+                        nextNode = networkMap.getValue(nextNode).first
+                    } else if (it == 'R') {
+                        nextNode = networkMap.getValue(nextNode).second
+                    }
+                    steps++
+                    if (nextNode == "ZZZ") {
+                        done = true
+                    }
                 }
             }
         }
+    } else {
+        // Find the LCM of all cycle lengths, which works because the graph seems to end in cycles for all starting
+        // points. Maybe this is not the case for all possible inputs though...
+        val periods = mutableListOf<Long>()
+        var nextNodes = networkMap.keys.filter { it.endsWith('A') }
+        while (!done) {
+            instructionsLine.forEach { instruction ->
+                if (!done) {
+                    if (instruction == 'L') {
+                        nextNodes = nextNodes.map { node -> networkMap.getValue(node).first }
+                    } else if (instruction == 'R') {
+                        nextNodes = nextNodes.map { node -> networkMap.getValue(node).second }
+                    }
+                    steps++
+                    if (nextNodes.filter { node -> node.endsWith('Z') }.isNotEmpty()) {
+                        periods.add(steps)
+                    }
+                    nextNodes = nextNodes.filter { node -> !node.endsWith('Z') }
+                    if (nextNodes.all { node -> node.endsWith('Z') }) {
+                        done = true
+                    }
+                }
+            }
+        }
+        steps = lcm(periods)
     }
 
     println(steps)
+}
+
+fun lcm(periods: List<Long>): Long {
+    var lcm = periods[0]
+    periods.subList(1, periods.size).forEach { lcm = lcm(lcm, it) }
+    return lcm
+}
+
+fun lcm(a: Long, b: Long): Long {
+    return a*b/gcd(a,b)
+}
+
+fun gcd(a: Long, b: Long): Long {
+    val (smaller, larger) = Pair(minOf(a,b), maxOf(a,b))
+    if (smaller == 0L) return larger
+    return gcd(smaller, larger % smaller)
 }
