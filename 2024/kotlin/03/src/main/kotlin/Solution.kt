30f30adc16ef1e51f8d78a6709b57a8c628a44a3
Albert Gustavsson <albert99gustavsson@gmail.com>
2024-12-03 06:45:09 +0100

    Solve 2024 day 3 part 2

--- 2024/kotlin/03/src/main/kotlin/Solution.kt
+++ 2024/kotlin/03/src/main/kotlin/Solution.kt
@@ -8,7 +8,20 @@ fun main(args:Array<String>) {
     val inputFileName = args[1]
 
     val fileContents: String = File(inputFileName).bufferedReader().readText()
-    val mulInstructions: List<String> = "mul\\([0-9]+,[0-9]+\\)".toRegex().findAll(fileContents).map { match -> match.value }.toList()
+    val mulMatches = "mul\\([0-9]+,[0-9]+\\)".toRegex().findAll(fileContents)
+
+    val dosAndDonts: Sequence<MatchResult> = "do(n't)?\\(\\)".toRegex().findAll(fileContents)
+    println("dosAndDonts: ${dosAndDonts.map { match -> match.value }.toList()}")
+
+    val enabledMulMatches = mulMatches.filter { match ->
+        val lastDoOrDont = dosAndDonts.lastOrNull { doOrDont -> doOrDont.range.last < match.range.first }
+        val enabled: Boolean = lastDoOrDont?.value?.equals("do()")?:true
+        enabled
+    }
+
+    println("Enabled muls: ${enabledMulMatches.map { match -> match.value }.toList()}")
+
+    val mulInstructions: List<String> = (if (part == 1) mulMatches else enabledMulMatches).map { match -> match.value }.toList()
 
     val multiplicationResults: List<Int> = mulInstructions.map { mulInstruction ->
         val firstMatch = "[0-9]+".toRegex().find(mulInstruction, 0)
