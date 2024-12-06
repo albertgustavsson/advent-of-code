d4b63b57c851e5c80e0b21e9f843b1e382a37907
Albert Gustavsson <albert99gustavsson@gmail.com>
2024-12-06 07:26:23 +0100

    Solve 2024 day 6 part 2

--- 2024/kotlin/06/src/main/kotlin/Solution.kt
+++ 2024/kotlin/06/src/main/kotlin/Solution.kt
@@ -9,23 +9,49 @@ fun main(args:Array<String>) {
 
     val grid: List<List<Char>> = File(inputFileName).bufferedReader().lines().map { line -> line.toMutableList() }.toList()
 
+    if (part == 1) {
+        val startLine = grid.indexOfFirst { line -> line.contains('^') }
+        val startColumn = grid[startLine].indexOf('^')
+        var guardPosition: Pair<Int, Int> = Pair(startLine, startColumn)
+        var guardDirection: Pair<Int, Int> = Pair(-1, 0)
+        val visitedPositions: MutableSet<Pair<Int, Int>> = mutableSetOf()
+
+        while (isInMap(grid, guardPosition)) {
+            visitedPositions.add(guardPosition)
+            if (isBlocked(grid, guardPosition, guardDirection))
+                guardDirection = guardDirection.rotateClockWise()
+            else
+                guardPosition = guardPosition.plus(guardDirection)
+        }
+
+        val numberOfVisitedPositions: Int = visitedPositions.size
+        println(numberOfVisitedPositions)
+    } else {
+        val potentialObstructionPositions: List<Pair<Int, Int>> = grid.flatMapIndexed { row, line -> line.mapIndexed { column, c -> if (c == '.') Pair(row, column) else null }.filterNotNull() }
+        val numberOfPositions: Int = potentialObstructionPositions.filter { pos -> guardGetsStuckInLoopWithAdditionalObstruction(grid, pos) }.size
+        println(numberOfPositions)
+    }
+}
+
+fun guardGetsStuckInLoopWithAdditionalObstruction(grid: List<List<Char>>, additionalObstruction: Pair<Int, Int>): Boolean {
     val startLine = grid.indexOfFirst { line -> line.contains('^') }
     val startColumn = grid[startLine].indexOf('^')
     var guardPosition: Pair<Int, Int> = Pair(startLine, startColumn)
     var guardDirection: Pair<Int, Int> = Pair(-1, 0)
-    val visitedPositions: MutableSet<Pair<Int, Int>> = mutableSetOf()
+    val visitedPositions: MutableMap<Pair<Int, Int>, MutableSet<Pair<Int, Int>>> = mutableMapOf()
 
     while (isInMap(grid, guardPosition)) {
-        visitedPositions.add(guardPosition)
-        if (isBlocked(grid, guardPosition, guardDirection))
+        val visitedPositionDirectionSet = visitedPositions.computeIfAbsent(guardPosition) { mutableSetOf() }
+        if (visitedPositionDirectionSet.contains(guardDirection))
+            return true
+
+        visitedPositionDirectionSet.add(guardDirection)
+        if (isBlocked(grid, guardPosition, guardDirection, additionalObstruction))
             guardDirection = guardDirection.rotateClockWise()
         else
             guardPosition = guardPosition.plus(guardDirection)
     }
-
-    val numberOfVisitedPositions: Int = visitedPositions.size
-    println(numberOfVisitedPositions)
-
+    return false
 }
 
 private fun Pair<Int, Int>.rotateClockWise(): Pair<Int, Int> {
@@ -42,9 +68,9 @@ private fun Pair<Int, Int>.plus(other: Pair<Int, Int>): Pair<Int, Int> {
     return Pair(first+other.first, second+other.second)
 }
 
-fun isBlocked(grid: List<List<Char>>, position: Pair<Int, Int>, direction: Pair<Int, Int>): Boolean {
+fun isBlocked(grid: List<List<Char>>, position: Pair<Int, Int>, direction: Pair<Int, Int>, additionalObstruction: Pair<Int, Int>? = null): Boolean {
     val pos = position.plus(direction)
-    return isInMap(grid, pos) && grid[pos.first][pos.second] == '#'
+    return isInMap(grid, pos) && (grid[pos.first][pos.second] == '#' || pos == additionalObstruction)
 }
 
 fun isInMap(grid: List<List<Char>>, position: Pair<Int, Int>): Boolean {
